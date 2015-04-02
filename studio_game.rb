74e7e0d7032f56732c253ec308fa81cc26a9bcc9
Marla Brizel <marlabrizel@gmail.com>
2015-04-02 09:34:19 -0600

    Add methods for name, health, and time

--- studio_game.rb
+++ studio_game.rb
@@ -1,20 +1,13 @@
-name1 = 'larry'
-health1 = 60
+def say_hello(name, health=100)
+	"I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
 
-name2 = 'curly'
-health2 = 125
+def time
+	current_time = Time.new
+	current_time.strftime("%I:%M:%S %p")
+end
 
-name3 = 'moe'
-health3 = 100
-
-name4 = 'shemp'
-health4 = 90
-
-puts "#{name1.capitalize} has a health of #{health1}."
-puts "#{name2.upcase} has a health of #{health2}."
-puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
-puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
-
-current_time = Time.new
-formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
-puts "The game started on #{formatted_time}"
+puts say_hello('larry', 60)
+puts say_hello('curly', 125)
+puts say_hello('moe')
+puts say_hello('shemp', 90)
