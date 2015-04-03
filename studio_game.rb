a7999667c8d7c05286ba091a507a02f52568a7d7
Marla Brizel <marlabrizel@gmail.com>
2015-04-03 13:42:36 -0600

    Add classes and additional methods

--- studio_game.rb
+++ studio_game.rb
@@ -1,13 +1,40 @@
-def say_hello(name, health=100)
-	"I'm #{name.capitalize} with a health of #{health} as of #{time}."
+class Player
+
+def initialize (name, health=100)
+	@name = name.capitalize
+	@health = health
 end
 
-def time
-	current_time = Time.new
-	current_time.strftime("%I:%M:%S %p")
+def to_s
+	"I'm #{@name} with a health of #{@health}"
 end
 
-puts say_hello('larry', 60)
-puts say_hello('curly', 125)
-puts say_hello('moe')
-puts say_hello('shemp', 90)
+def blam
+	@health -=10
+	puts "#{@name} got blammed!"
+end
+
+def w00t
+	@health +=15
+	puts "#{@name} got w00ted!"
+end
+
+end
+
+
+player1 = Player.new("moe")
+puts player1
+player1.w00t
+puts player1
+
+player2 = Player.new("larry")
+puts player2
+player2.blam
+puts player2
+
+player3 = Player.new("curly")
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
