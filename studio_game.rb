f0e70622f8ba3eebc2aca7bd7844abb0e8a8cfc1
Marla Brizel <marlabrizel@gmail.com>
2015-04-08 16:24:48 -0600

    add attributes

--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,23 @@
 class Player
 
+	attr_reader :health
+	attr_accessor :name
+
+def name=(new_name)
+	@name = new_name.capitalize
+end
+
+def score
+	@health + @name.length
+end
+
 def initialize (name, health=100)
 	@name = name.capitalize
 	@health = health
 end
 
 def to_s
-	"I'm #{@name} with a health of #{@health}"
+	"I'm #{@name} with a health of #{@health} and a score of #{score}."
 end
 
 def blam
@@ -24,17 +35,12 @@ end
 
 player1 = Player.new("moe")
 puts player1
-player1.w00t
-puts player1
 
 player2 = Player.new("larry")
 puts player2
-player2.blam
+player2.name = "lawrence"
 puts player2
 
 player3 = Player.new("curly")
-puts player3
 player3.blam
-puts player3
-player3.w00t
 puts player3
