0c7675c3b947542827248e2b3b1f305846134866
Marla Brizel <marlabrizel@gmail.com>
2015-04-28 16:31:10 -0600

    refactor code to incorporate GameTurn module

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'game_turn'
 
 class Game
 
@@ -21,16 +22,7 @@ class Game
 		end
 
 		@players.each do |player|
-			die = Die.new
-			number_rolled = die.roll
-			case number_rolled
-			when 1..2
-				player.blam
-			when 3..4
-				puts "#{player.name} was skipped."
-			else
-				player.w00t
-			end
+			GameTurn.take_turn(player)
 			puts player
 		end
 	end
