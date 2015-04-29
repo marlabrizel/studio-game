1d524cc10de82bf47d92310c4f3c002e17c5edfd
Marla Brizel <marlabrizel@gmail.com>
2015-04-28 18:54:51 -0600

    update tests to account for multiple rounds of game play

--- game_spec.rb
+++ game_spec.rb
@@ -14,26 +14,26 @@ describe Game do
 	it "w00ts a player when a high number is rolled" do
 		allow_any_instance_of(Die).to receive(:roll).and_return(5)
 
-		@game.play 
+		@game.play(2) 
 
-		expect(@player.health).to eq(@initial_health + 15)
+		expect(@player.health).to eq(@initial_health + 15 * 2)
 	end
 
 	it "skips a player when a medium number is rolled" do
 		allow_any_instance_of(Die).to receive(:roll).and_return(3)
 
-		@game.play
+		@game.play(2)
 
 		expect(@player.health).to eq(@initial_health)
 	end
 
-	
+
 	it "blams a player when a low number is rolled" do
 		allow_any_instance_of(Die).to receive(:roll).and_return(1)
 
-		@game.play
+		@game.play(2)
 
-		expect(@player.health).to eq(@initial_health - 10)
+		expect(@player.health).to eq(@initial_health - 10 *2)
 	end
 
 end
