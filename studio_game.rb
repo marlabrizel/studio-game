710dca56b6526c4d778f7bfdb7e056a2bb998055
Marla Brizel <marlabrizel@gmail.com>
2015-05-01 11:11:12 -0600

    remove dogs portion of game, add high scores

--- studio_game.rb
+++ studio_game.rb
@@ -10,14 +10,3 @@ knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
 knuckleheads.play(3)
 knuckleheads.print_stats
-
-player5 = Player.new("riley", 110)
-player6 = Player.new("keller", 95)
-player7 = Player.new("penny", 140)
-
-dogs = Game.new("dogs")
-dogs.add_player(player5)
-dogs.add_player(player6)
-dogs.add_player(player7)
-dogs.play(1)
-dogs.print_stats
