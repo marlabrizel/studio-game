9ee7cfe8753e7ffd0d43fc765bb4cd62bdacc82e
Marla Brizel <marlabrizel@gmail.com>
2015-05-04 14:04:12 -0600

    add treasure accumulation, storage, tallying functionality

--- player.rb
+++ player.rb
@@ -15,6 +15,7 @@ def initialize (name, health=100)
 	self.name = name
 	@name = name.capitalize
 	@health = health
+	@found_treasures = Hash.new(0)
 end
 
 def to_s
@@ -37,6 +38,16 @@ end
 
 def <=>(other)
 	other.score <=> score
+end
+
+def found_treasure(treasure)
+	@found_treasures[treasure.name] += treasure.points
+	puts"#{name} found a #{treasure.name} worth #{treasure.points} points."
+	puts "#{name}'s treasures: #{@found_treasures}"
+end
+
+def points
+	@found_treasures.values.reduce(0, :+)
 end
 
 end
