be80ac284bcece30e2a7102c3107fa0a548bfb0b
Marla Brizel <marlabrizel@gmail.com>
2015-05-04 20:16:34 -0600

    make it so that a treasure is found on every turn, regardless of number rolled

--- game_turn.rb
+++ game_turn.rb
@@ -5,17 +5,17 @@ require_relative 'treasure_trove'
 module GameTurn
 	def self.take_turn(player)
 		die = Die.new
-			number_rolled = die.roll
-			case number_rolled
-			when 1..2
-				player.blam
-			when 3..4
-				puts "#{player.name} was skipped."
-			else
-				player.w00t
-
-			treasure = TreasureTrove.random
-			puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points"
+		number_rolled = die.roll
+		case number_rolled
+		when 1..2
+			player.blam
+		when 3..4
+			puts "#{player.name} was skipped."
+		else
+			player.w00t
 		end
+		
+		treasure = TreasureTrove.random
+		player.found_treasure(treasure)
 	end
 end
