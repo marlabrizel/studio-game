7eee1f4e0b15e5a29d22069a078900009baa2403
Marla Brizel <marlabrizel@gmail.com>
2015-05-07 12:04:09 -0600

    add method to sum treasure points on per treasure basis

--- player.rb
+++ player.rb
@@ -1,54 +1,61 @@
-class Player
+require_relative 'treasure_trove'
 
+class Player
 	attr_reader :health
 	attr_accessor :name
 
-def name=(new_name)
-  @name = new_name.capitalize
- end
+	def name=(new_name)
+  		@name = new_name.capitalize
+ 	end
 
- def score
-	@health + points
-end
+	def score
+		@health + points
+	end
 
-def initialize (name, health=100)
-	self.name = name
-	@name = name.capitalize
-	@health = health
-	@found_treasures = Hash.new(0)
-end
+	def initialize (name, health=100)
+		self.name = name
+		@name = name.capitalize
+		@health = health
+		@found_treasures = Hash.new(0)
+	end
 
-def to_s
-	"I'm #{@name} with health = #{@health}, points = #{points}, and score = #{score}."
-end
+	def to_s
+		"I'm #{@name} with health = #{@health}, points = #{points}, and score = #{score}."
+	end
 
-def blam
-	@health -=10
-	puts "#{@name} got blammed!"
-end
+	def blam
+		@health -=10
+		puts "#{@name} got blammed!"
+	end
 
-def w00t
-	@health +=15
-	puts "#{@name} got w00ted!"
-end
+	def w00t
+		@health +=15
+		puts "#{@name} got w00ted!"
+	end
 
-def strong?
-	@health > 100
-end
+	def strong?
+		@health > 100
+	end
 
-def <=>(other)
-	other.score <=> score
-end
+	def <=>(other)
+		other.score <=> score
+	end
 
-def found_treasure(treasure)
-	@found_treasures[treasure.name] += treasure.points
-	puts"#{name} found a #{treasure.name} worth #{treasure.points} points."
-	puts "#{name}'s treasures: #{@found_treasures}"
-end
+	def found_treasure(treasure)
+		@found_treasures[treasure.name] += treasure.points
+		puts"#{name} found a #{treasure.name} worth #{treasure.points} points."
+		puts "#{name}'s treasures: #{@found_treasures}"
+	end
 
-def points
-	@found_treasures.values.reduce(0, :+)
-end
+	def points
+		@found_treasures.values.reduce(0, :+)
+	end
+
+	def each_found_treasure
+		@found_treasures.each do |name, points|
+			yield Treasure.new(name, points)
+		end
+	end
 
 end
 
