54b64dd7511170f22338228253d8ff42f73d14bb
Marla Brizel <marlabrizel@gmail.com>
2015-05-07 12:04:26 -0600

    add tests for method to sum treasure points on per treasure basis

--- player_spec.rb
+++ player_spec.rb
@@ -95,4 +95,26 @@ require_relative 'treasure_trove'
 			expect(@player.points).to eq(500)
 		end
 
+		it "yields each found treasure and its total points" do
+			@player.found_treasure(Treasure.new(:skillet, 100))
+			@player.found_treasure(Treasure.new(:skillet, 100))
+			@player.found_treasure(Treasure.new(:hammer, 50))
+			@player.found_treasure(Treasure.new(:bottle, 5))
+			@player.found_treasure(Treasure.new(:bottle, 5))
+			@player.found_treasure(Treasure.new(:bottle, 5))
+			@player.found_treasure(Treasure.new(:bottle, 5))
+			@player.found_treasure(Treasure.new(:bottle, 5))
+
+			yielded = []
+			@player.each_found_treasure do |treasure|
+				yielded << treasure
+			end
+
+			expect(yielded).to eq[
+				Treasure.new(:skillet, 200),
+				Treasure.new(:hammer, 50),
+				Treasure.new(:bottle, 25),
+			]
+		end
+
 	end
