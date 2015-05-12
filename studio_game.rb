d18bf4fa40a6ca228d4c332042de03ae5819fe2e
Marla Brizel <marlabrizel@gmail.com>
2015-05-12 14:25:17 -0600

    add clumsy player

--- studio_game.rb
+++ studio_game.rb
@@ -3,9 +3,11 @@ require_relative 'game'
 #player1 = Player.new("moe")
 #player2 = Player.new("larry", 60)
 #player3 = Player.new("curly", 125)
+klutz = ClumsyPlayer.new("klutz", 105)
 
 knuckleheads = Game.new("knuckleheads")
 knuckleheads.load_players(ARGV.shift || "players.csv")
+knuckleheads.add_player(klutz)
 
 loop do
 	puts "\nHow many rounds? ('quit' to exit)"
