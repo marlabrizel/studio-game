2e905a039df59e368d4c2681b9f87c0a054ba49a
Marla Brizel <marlabrizel@gmail.com>
2015-05-13 16:37:51 -0600

    initial commit

---
+++ playable.rb
@@ -0,0 +1,15 @@
+module Playable
+	def blam
+		@health -=10
+		puts "#{@name} got blammed!"
+	end
+
+	def w00t
+		@health +=15
+		puts "#{@name} got w00ted!"
+	end
+
+	def strong?
+		@health > 100
+	end
+end
