a56a757f9d4dfd1718ab68b1fb4006098f343805
Marla Brizel <marlabrizel@gmail.com>
2015-05-13 16:37:32 -0600

    add mixin

--- die.rb
+++ die.rb
@@ -1,4 +1,8 @@
+require_relative 'auditable'
+
 class Die
+	include Auditable
+
 	attr_reader :number
 
 	def initialize
@@ -7,6 +11,9 @@ class Die
 
 	def roll
 		@number = rand(1..6)
+		audit
+		@number
 	end
 
 end
+
